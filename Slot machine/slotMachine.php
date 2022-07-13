8851f24749e424cce2406a54bcd643b277eeccfc
Jānis Šņoriņš <104723218+janisjuniors@users.noreply.github.com>
2022-07-13 09:16:19 +0300

    Update slotMachine.php

--- Slot machine/slotMachine.php
+++ Slot machine/slotMachine.php
@@ -4,26 +4,9 @@ $wallet = 100;
 
 $board = [
     [' ', ' ', ' ', ' ', ' '],
-
     [' ', ' ', ' ', ' ', ' '],
-
     [' ', ' ', ' ', ' ', ' ']
 ];
-
-function checkFreeSpaces(array $board): int
-{
-    $freeSpaces = 15;
-
-    for ($i = 0; $i < 3; $i++){
-        for ($j = 0; $j < 5; $j++){
-            if ($board[$i][$j] != ' '){
-                $freeSpaces--;
-            }
-        }
-    }
-
-    return $freeSpaces;
-}
 
 function displayBoard(array $board)
 {
@@ -32,179 +15,106 @@ function displayBoard(array $board)
     echo "\t" . implode('  ', $board[2]) . PHP_EOL;
 }
 
-$symbols = ['F','K', 'A', 'W', 'B', 'Q', '*'];
-$multipliers = [0.5, 1.5, 2, 3, 4, 5, 50];
-$winningSymbol = ' ';
+$symbols = [
+    "\e[1;37;40mK\e[0m", "\e[1;33;40mA\e[0m", "\e[0;32;40mB\e[0m",
+    "\e[0;34;40m*\e[0m", "\e[0;31;40m$\e[0m"
+];
 
-function winLine($board, $winningSymbol)
-{
-    for ($i = 0; $i < 3; $i++) {
-        // ALL HORIZONTAL SYMBOLS THE SAME
-        if ($board[$i][0] === $board[$i][1]
-            && $board[$i][0] === $board[$i][2]
-            && $board[$i][0] === $board[$i][3]
-            && $board[$i][0] === $board[$i][4]) {
-            return $board[$i][0];
-        }
-        // 4 HORIZONTAL SYMBOLS THE SAME FROM 0
-        if ($board[$i][0] === $board[$i][1]
-            && $board[$i][1] === $board[$i][2]
-            && $board[$i][2] === $board[$i][3]) {
-            return $board[$i][0];
-        }
-        // 4 HORIZONTAL SYMBOLS THE SAME FROM 1
-        if ($board[$i][1] === $board[$i][2]
-            && $board[$i][2] === $board[$i][3]
-            && $board[$i][3] === $board[$i][4]) {
-            return $board[$i][1];
-        }
-        // 3 HORIZONTAL SYMBOLS THE SAME FROM 0
-        if ($board[$i][0] === $board[$i][1]
-            && $board[$i][1] === $board[$i][2]) {
-            return $board[$i][0];
-        }
-        // 3 HORIZONTAL SYMBOLS THE SAME FROM 1
-        if ($board[$i][1] === $board[$i][2]
-            && $board[$i][2] === $board[$i][3]) {
-            return $board[$i][1];
-        }
-        // 3 HORIZONTAL SYMBOLS THE SAME FROM 2
-        if ($board[$i][2] === $board[$i][3]
-            && $board[$i][3] === $board[$i][4]) {
-            return $board[$i][2];
-        }
-        // 1ST ZIGZAG PAY LINE SYMBOLS THE SAME
-        if ($board[0][0] === $board[1][1]
-            && $board[1][1] === $board[2][2]
-            && $board[2][2] === $board[1][3]
-            && $board[1][3] === $board[0][4]) {
-            return $board[0][0];
-        }
-        // 1ST ZIGZAG PAY LINE 4 SYMBOLS THE SAME
-        if ($board[0][0] === $board[1][1]
-            && $board[1][1] === $board[2][2]
-            && $board[2][2] === $board[1][3]) {
-            return $board[0][0];
-        }
-        // 1ST ZIGZAG PAY LINE LAST 4 SYMBOLS THE SAME
-        if ($board[1][1] === $board[2][2]
-            && $board[2][2] === $board[1][3]
-            && $board[1][3] === $board[0][4]) {
-            return $board[1][1];
-        }
-
-        // 1ST ZIGZAG PAY LINE LAST 3 SYMBOLS THE SAME
-        if ($board[0][0] === $board[1][1]
-            && $board[1][1] === $board[2][2]) {
-            return $board[0][0];
-        }
-
-        // 1ST ZIGZAG PAY LINE LAST 2ND 3 SYMBOLS THE SAME
-        if ($board[1][1] === $board[2][2]
-            && $board[2][2] === $board[1][3]) {
-            return $board[1][1];
-        }
-
-        // 1ST ZIGZAG PAY LINE LAST 3RD 3 SYMBOLS THE SAME
-        if ($board[2][2] === $board[1][3]
-            && $board[1][3] === $board[0][4]) {
-            return $board[2][2];
-        }
-
-        // 2ND ZIGZAG PAY LINE SYMBOLS THE SAME
-        if ($board[2][0] === $board[1][1]
-            && $board[1][1] === $board[0][2]
-            && $board[0][2] === $board[1][3]
-            && $board[1][3] === $board[2][4]) {
-            return $board[2][0];
-        }
-
-        // 2ND ZIGZAG PAY LINE 4 SYMBOLS THE SAME
-        if ($board[2][0] === $board[1][1]
-            && $board[1][1] === $board[0][2]
-            && $board[0][2] === $board[1][3]) {
-            return $board[2][0];
-        }
-
-        // 2ND ZIGZAG PAY LINE 4 SYMBOLS THE SAME
-        if ($board[1][1] === $board[0][2]
-            && $board[0][2] === $board[1][3]
-            && $board[1][3] === $board[2][4]) {
-            return $board[1][1];
-        }
-
-        // 2ND ZIGZAG PAY LINE 3 SYMBOLS THE SAME
-        if ($board[2][0] === $board[1][1]
-            && $board[1][1] === $board[0][2]) {
-            return $board[2][0];
-        }
-
-        // 2ND ZIGZAG PAY LINE 2ND 3 SYMBOLS THE SAME
-        if ($board[1][1] === $board[0][2]
-            && $board[0][2] === $board[1][3]) {
-            return $board[1][1];
-        }
-
-        // 2ND ZIGZAG PAY LINE 3RD 3 SYMBOLS THE SAME
-        if ($board[0][2] === $board[1][3]
-            && $board[1][3] === $board[2][4]) {
-            return $board[0][2];
-        }
-    }
-
-    return $winningSymbol;
-}
+$multipliers = [1.2, 1.6 , 2, 2.5, 5];
 
 echo 'Welcome to slots game!' . PHP_EOL;
 echo PHP_EOL;
 echo "Your balance is $wallet" . PHP_EOL;
 $bet = (int)readline('Choose your bet size: ');
-$choice = (string)readline('Spin? (Y/N): ');
+$choice = (string)readline('Spin? (Enter/any): ');
+
+
+function checkingWinLines(array $timesChar, string $symbolString, int $bet, array $multipliers, array $symbols): float
+{
+    foreach ($timesChar as $char => $times) {
+        if (strpos($symbolString, $char . $char . $char . $char . $char) !== false) {
+            $keyForWinSymbol = array_search($char, $symbols);
+            return $bet * $multipliers[$keyForWinSymbol] * 1.5;
+        } elseif (strpos($symbolString, $char . $char . $char . $char) !== false) {
+            $keyForWinSymbol = array_search($char, $symbols);
+            return $bet * $multipliers[$keyForWinSymbol] * 1.4;
+        } elseif (strpos($symbolString, $char . $char . $char) !== false) {
+            $keyForWinSymbol = array_search($char, $symbols);
+            return $bet * $multipliers[$keyForWinSymbol] * 1.3;
+        }
+    }
+    return  0;
+}
+
+function swap(&$x, &$y) {
+    $tmp = $x;
+    $x = $y;
+    $y = $tmp;
+}
 
 while (true) {
-    while (checkFreeSpaces($board) > 0) {
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 5; $j++) {
-                $randomChar = $symbols[array_rand($symbols)];
-                $board[$i][$j] = $randomChar;
-            }
+    for ($i = 0; $i < count($board); $i++) {
+        for ($j = 0; $j < count($board[$i]); $j++) {
+            $board[$i][$j] = $symbols[rand(0, count($symbols) - 1)];
         }
-        displayBoard($board);
+    }
 
+    displayBoard($board);
 
-        if (winLine($board, $winningSymbol) != ' '){
-            $keyForWinSymbol = array_search(winLine($board,$winningSymbol), $symbols);
-            $multiplier = $multipliers[$keyForWinSymbol];
-            $winningSum = $bet * $multiplier;
-            echo 'Multiplier you got is ' . $multiplier . PHP_EOL;
-            echo 'You won ' . $winningSum . PHP_EOL;
-            $wallet += $winningSum;
-        } else {
-            echo 'You lost this time!' . PHP_EOL;
-            $wallet -= $bet;
-            if ($wallet <= 0) {
-                echo 'You dont have any more money!' . PHP_EOL;
-                exit;
-            } elseif ($wallet < $bet){
-                echo 'You dont have enough money!' . PHP_EOL;
-                exit;
-            }
+    $moneyWonTotalSpin = 0;
+    // Horizontal Lines
+    for ($i = 0; $i < count($board); $i++) {
+        $symbolString = implode('', $board[$i]);
+        $timesChar = array_count_values($board[$i]);
+        $moneyWonTotalSpin += checkingWinLines($timesChar, $symbolString, $bet, $multipliers, $symbols);
+    }
+
+    // 1st Diagonal
+    $diagonal = [];
+    $diagonalLineArr = array_merge(...$board);
+    foreach ($diagonalLineArr as $key => $symbol) {
+        if ($key === 0 || $key === 4 || $key === 8 || $key === 6 || $key === 12) {
+            $diagonal[] = $symbol;
         }
-        echo "Your balance is $wallet" . PHP_EOL;
+    }
+    swap($diagonal[1], $diagonal[2]);
+    swap($diagonal[2], $diagonal[4]);
 
-        $choice = (string)readline('Spin? (Y/N): ');
+    $timesChar = array_count_values($diagonal);
+    $diagonal = implode('', $diagonal);
+    $moneyWonTotalSpin += checkingWinLines($timesChar, $diagonal, $bet, $multipliers, $symbols);
 
-        if ($choice === 'Y') {
-            for ($i = 0; $i < 3; $i++) {
-                for ($j = 0; $j < 5; $j++) {
-                    $board[$i][$j] = ' ';
-                }
-            }
-        } else {
-            exit;
+
+    // 2nd Diagonal
+    $diagonal = [];
+    $diagonalLineArr = array_merge(...$board);
+    foreach ($diagonalLineArr as $key => $symbol) {
+        if ($key === 2 || $key === 6 || $key === 8 || $key === 10 || $key === 14) {
+            $diagonal[] = $symbol;
         }
+    }
+    swap($diagonal[0], $diagonal[3]);
+    swap($diagonal[2], $diagonal[3]);
 
+    $timesChar = array_count_values($diagonal);
+    $diagonal = implode('', $diagonal);
+    $moneyWonTotalSpin += checkingWinLines($timesChar, $diagonal, $bet, $multipliers, $symbols);
 
+    if ($moneyWonTotalSpin == 0) {
+        $wallet -= $bet;
+        echo 'You lost!' . PHP_EOL;
+    } else {
+        $wallet += $moneyWonTotalSpin;
+        echo 'Money won: ' . $moneyWonTotalSpin . PHP_EOL;
+    }
+    echo 'Your balance: ' . $wallet . PHP_EOL;
+
+    $choice = (string)readline('Spin? (Enter/any): ');
+    if (strlen($choice) === 0)  {
+        continue;
+    } else {
+        exit;
     }
 }
+
 
